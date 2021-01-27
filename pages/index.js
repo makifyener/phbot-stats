0693a9848504fcc408564db80aef5edddd989724
Akif Yener <yener@terraeds.com>
2021-01-27 18:01:02 -0500

    home and about pages added

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+const Index = () => (
+    <>
+        <h1>Hello from Next JS</h1>
+        <Link href="/about"><a>About</a></Link>
+    </>
+)
+
+export default Index
